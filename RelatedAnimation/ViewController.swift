a6d436204b7c0ed439501d2cb6a7b0f9276e23e1
Margarita Slesareva <slesareva.m.a@yandex.ru>
2024-02-08 19:11:01 +0300

    rotate and move square

--- RelatedAnimation/ViewController.swift
+++ RelatedAnimation/ViewController.swift
@@ -8,22 +8,47 @@
 import UIKit
 
 private enum Metrics {
-    static let squareHeight: CGFloat = 100
+    static let squareSize: CGSize = .init(width: 100, height: 100)
     static let squareColor: UIColor = .systemBlue
+    static let cornerRadius: CGFloat = 10
 }
 
 final class ViewController: UIViewController {
     
+    private var viewIsLayedOutFirstTime = false
+
     private let squareView = UIView()
     private let slider = UISlider()
-
+    private let animator = UIViewPropertyAnimator(duration: 0.3, curve: .linear)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setup()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        view.layoutMarginsDidChange()
+        
+        setupSubviewsIfNeeded()
+    }
+        
+    private func setup() {
         addViews()
+        configureViews()
+        configureAnimator()
+    }
+    
+    private func setupSubviewsIfNeeded() {
+        guard !viewIsLayedOutFirstTime else {
+            return
+        }
+        
         setFrame()
         addConstraints()
-        configureViews()
+        
+        viewIsLayedOutFirstTime = true
     }
     
     private func addViews() {
@@ -35,19 +60,15 @@ final class ViewController: UIViewController {
     }
     
     private func setFrame() {
-        squareView.frame = CGRect(
-            x: squareView.layoutMargins.left,
-            y: Metrics.squareHeight,
-            width: Metrics.squareHeight,
-            height:  Metrics.squareHeight
-        )
+        let origin = CGPoint(x: view.layoutMargins.left, y: Metrics.squareSize.height)
+        squareView.frame = CGRect(origin: origin, size: Metrics.squareSize)
     }
     
     private func addConstraints() {
         NSLayoutConstraint.activate([
-            slider.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: slider.layoutMargins.left),
-            slider.topAnchor.constraint(equalTo: squareView.bottomAnchor, constant: 20),
-            slider.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -slider.layoutMargins.right)
+            slider.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.layoutMargins.left),
+            slider.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -view.frame.height / 5),
+            slider.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -view.layoutMargins.right)
         ])
     }
     
@@ -55,18 +76,33 @@ final class ViewController: UIViewController {
         view.backgroundColor = .systemBackground
         
         squareView.backgroundColor = Metrics.squareColor
-        squareView.layer.cornerRadius = 10
+        squareView.layer.cornerRadius = Metrics.cornerRadius
         
-        slider.addTarget(self, action: #selector(squareRotate), for: .valueChanged)
-        slider.minimumValue = Float(squareView.frame.origin.x)
-        slider.maximumValue = Float(view.frame.width - squareView.layoutMargins.right - squareView.frame.width)
+        slider.addTarget(self, action: #selector(updateAnimation), for: .valueChanged)
+        slider.addTarget(self, action: #selector(animate), for: .touchUpInside)
+        slider.minimumValue = 0.0
+        slider.maximumValue = 1.0
     }
     
-    @objc private func squareRotate() {
-        let animator = UIViewPropertyAnimator(duration: 0.3, curve: .linear) {
-            self.squareView.frame.origin.x = CGFloat(self.slider.value)
+    private func configureAnimator() {
+        animator.addAnimations {
+            self.squareView.frame = CGRect(
+                x: self.view.frame.width - self.squareView.frame.width * 1.5 - self.view.layoutMargins.right,
+                y: self.squareView.frame.minY,
+                width: self.squareView.frame.width * 1.5,
+                height: self.squareView.frame.height * 1.5
+            )
+            self.squareView.transform = CGAffineTransform(rotationAngle: .pi / 2)
+            self.animator.pausesOnCompletion = true
         }
-        
+    }
+    
+    @objc private func updateAnimation() {
+        animator.fractionComplete = CGFloat(slider.value)
+    }
+    
+    @objc private func animate() {
+        slider.setValue(slider.maximumValue, animated: true)
         animator.startAnimation()
     }
 }
